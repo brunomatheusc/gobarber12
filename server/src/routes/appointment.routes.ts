d0601a6dee459ec69eedb12f1909ffdf263df07a
bmatheusc <desenvolvimento4@sistemavitto.com.br>
2020-06-20 16:16:55 -0300

    First commit

---
+++ server/src/routes/appointment.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+
+import AppointmentsController from './../controllers/AppointmentsController';
+
+const appointmentsRoutes = Router();
+
+appointmentsRoutes.get('/', AppointmentsController.read);
+appointmentsRoutes.post('/', AppointmentsController.create);
+
+export default appointmentsRoutes;
