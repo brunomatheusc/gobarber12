d263a26a21fb7540fd2151c5331c3dfcfe2d61e4
bmatheusc <desenvolvimento4@sistemavitto.com.br>
2020-08-11 16:48:06 -0300

    [Test] Users :: adding tests for create users service

---
+++ server/src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -0,0 +1,72 @@
+import path from 'path';
+import fs from 'fs';
+import AppError from '../../../../shared/errors/AppError';
+
+import uploadConfig from '../../../../config/upload';
+import IUserRepository from 'modules/users/repositories/IUserRepository';
+import User from 'modules/users/typeorm/entities/User';
+import { uuid } from 'uuidv4';
+
+interface UserDTO {
+	name: string;
+	email: string;
+	password: string;
+}
+
+interface AvatarDTO {
+	id: string;
+	avatar: string;
+}
+
+export default class UsersRepository implements IUserRepository {
+	private users: User[] = [];
+	
+	public async findById(id: string): Promise<User | undefined> {
+		return this.users.find(user => user.id == id);
+	}
+	
+	public async findByEmail(email: string): Promise<User | undefined>{
+		return this.users.find(user => user.email == email);
+	}
+
+	public async create(userData: UserDTO): Promise<User> {
+		const user = new User();
+
+		Object.assign(user, { id: uuid() }, userData);
+
+		this.users.push(user);
+
+		return user;
+	}
+
+	public async updateUserAvatar({ id, avatar }: AvatarDTO): Promise<User> {
+		const user = await this.findById(id);
+
+		if (!user) {
+			throw new AppError('Only authenticade users can change avatar.', 401);
+		}
+
+		if (user.avatar) {
+			const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+			const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath); 
+
+			if (userAvatarFileExists) {
+				await fs.promises.unlink(userAvatarFilePath);
+			}
+		}
+
+		user.avatar = avatar;
+
+		await this.save(user);
+
+		return user;
+	}
+
+	public async save(user: User): Promise<User> {
+		const findIndex = this.users.findIndex(findUser => findUser.id == user.id);
+
+		this.users[findIndex] = user;
+
+		return user;
+	}
+}
---
+++ server/src/modules/users/services/CreateUserService.spect.ts
@@ -0,0 +1,36 @@
+import "reflect-metadata";
+import CreateUserService from './CreateUserService';
+import FakeUsersRepository from './../repositories/fakes/FakeUsersRepository';
+import AppError from './../../../shared/errors/AppError';
+
+describe('CreateUser', () => {
+	it('should be able to create a new appointment', async () => {
+		const fakeUsersRepository = new FakeUsersRepository();
+		const createUser = new CreateUserService(fakeUsersRepository);
+
+		const user = await createUser.execute({
+			name: 'John Doe',
+			email: 'johndoe@example.com',
+			password: '123456'
+		});
+
+		expect(user).toHaveProperty('id');
+	});
+
+	it('should not be able to create a new user with same e-mail from another', async () => {
+		const fakeUsersRepository = new FakeUsersRepository();
+		const createUser = new CreateUserService(fakeUsersRepository);
+
+		const user = await createUser.execute({
+			name: 'John Doe',
+			email: 'johndoe@example.com',
+			password: '123456'
+		});
+
+		expect(createUser.execute({
+			name: 'John Doe',
+			email: 'johndoe@example.com',
+			password: '123456'
+		})).rejects.toBeInstanceOf(Error);
+	});
+});
